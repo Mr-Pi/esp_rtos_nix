ab761e033fc538d06ac7456e040a45e35c6872c7
Janne Heß <janne@hess.ooo>
2020-02-22 16:39:14 +0100

    Fix menuconfig

--- shell.nix
+++ shell.nix
@@ -13,10 +13,15 @@ stdenv.mkDerivation {
       pyelftools
     ]))
     (callPackage ./toolchain.nix {})
+    bison
+    flex
+    gperf
+    ncurses
   ];
 
   shellHook = ''
     export IDF_PATH="''${PWD}/ESP8266_RTOS_SDK"
+    export NIX_CFLAGS_LINK=-lncurses
 
     if ! [[ -d "$IDF_PATH" ]]; then
       git clone https://github.com/espressif/ESP8266_RTOS_SDK
